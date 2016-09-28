ab977fb3da7beaa50b1c81106a09261754b217bc
Mircea Mihalea <mircea@mihalea.ro>
2016-09-28 19:56:16 +0300

    Added email and multiple users
    Emails will be sent to users which fail to satisfy the dare program.
    Multiple users can be monitored.
    A config file has been created which the user needs to edit.

--- dare.py
+++ dare.py
@@ -4,6 +4,9 @@ import urllib.request
 import sys
 import time
 import json
+import smtplib
+from email.mime.text import MIMEText
+from config import *
 
 class DoesNotCount(Exception):
     pass
@@ -37,9 +40,23 @@ def has_committed(user):
 
     return False
 
+# Send an email to the recipient using the local server
+def mail(user):
+	msg = MIMEText(user + " has dared not to commit daily!")
+	msg["Subject"] = "Dare violation!"
+	msg["From"] = FROM
+	msg["To"] = RECIPIENT
+
+	s = smtplib.SMTP("localhost")
+	s.send_message(msg)
+	s.quit()
+
+# If the user has not committed, send an email to the recipient
+def check(user):
+    if not has_committed(user):
+        mail(user)
+
 # This must be run with the username of the user to be checked as an argument
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Please provide a username as the only argument")
-    else:
-        print(has_committed(sys.argv[1]))
+    for user in USER:
+    	check(user)
